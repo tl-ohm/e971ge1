992df1e0ce17c2616dd75aaa35202471302c8395
omsl <94546656+tl-ohm@users.noreply.github.com>
2025-08-15 00:56:27 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="client/static"), name="static")
+templates = Jinja2Templates(directory="client/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def landing_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.exception_handler(Exception)
+async def handle_all_errors(request: Request, exc: Exception):
+    return RedirectResponse(url="/")
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: StarletteHTTPException):
+    return RedirectResponse(url="/")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app)
